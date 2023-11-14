045c0dc6c983b1b21603732595374e927ddd7ec7
Akaash <akaashandrew1@gmail.com>
2023-11-14 11:24:31 +0530

    add_product updated with php and db

--- add_product.php
+++ add_product.php
@@ -4,28 +4,22 @@ $username = "akaash";
 $password = "Achielles@20";
 $dbname = "miniproject";
 $conn = new mysqli($servername, $username, $password, $dbname);
-
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
-
 $name = $_POST['name'];
 $description = $_POST['description'];
 $duration = $_POST['duration'];
-
-$query = "SELECT MAX(product_id) FROM add_product";
+$query = "SELECT MAX(product_id) FROM product_details";
 $result = $conn->query($query);
 $row = $result->fetch_assoc();
 $nextProductNumber = $row['MAX(product_id)'] + 1;
-
-$sql1 = "INSERT INTO add_product (product_id, name, description, duration) VALUES ($nextProductNumber, '$name', '$description', '$duration')";
-
+$sql1 = "INSERT INTO product_details (product_id, name, description, duration) VALUES ($nextProductNumber,'$name', '$description', '$duration')";
 if ($conn->query($sql1) === TRUE) {
-    echo '<script>alert("Successful");</script>';
+    header("Location: schemepage.php");
     exit();
 } else {
     echo "Error: " . $sql1 . "<br>" . $conn->error;
 }
-
 $conn->close();
 ?>
