ba6dd5a0ce5a3c164abc19b647a7da3f9f8037f8
Akaash <akaashandrew1@gmail.com>
2023-11-16 00:35:10 +0530

    basiclogin updated to user_login with php

---
+++ user_login.php
@@ -0,0 +1,35 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $phone = $_POST["phone"];
+    $password = $_POST["password"];
+    $host = "localhost"; 
+    $username = "akaash"; 
+    $password_db = "Achielles@20"; 
+    $database = "miniproject"; 
+    $conn = new mysqli($host, $username, $password_db, $database);
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+    $sql = "SELECT * FROM user_details WHERE mobile_number='$phone' AND password='$password'";
+    $result = $conn->query($sql);
+    if ($result->num_rows == 1) {
+        $row = $result->fetch_assoc();
+        $_SESSION['role'] = $row['role'];
+        
+        if ($_SESSION['role'] === 'admin') {
+            header('Location: homepage.php');
+        } 
+        else {
+            header('Location: user_register.html');
+        }
+    } 
+    else {
+        echo '<script type="text/javascript">alert("Invalid username or password.");';
+        echo 'window.location.href = "user_login.html";</script>';
+        exit;
+    }
+    
+    $conn->close();
+}
+?>
+
