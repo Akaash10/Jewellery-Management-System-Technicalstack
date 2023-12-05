f4824e2441591a31b05d7203d4cc6b1c1d50e3de
Akaash <akaashandrew1@gmail.com>
2023-12-05 13:04:08 +0530

    sql

---
+++ miniproject.sql
@@ -0,0 +1,94 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Dec 05, 2023 at 08:29 AM
+-- Server version: 10.4.27-MariaDB
+-- PHP Version: 8.2.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `miniproject`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `personal_details`
+--
+
+CREATE TABLE `personal_details` (
+  `user_id` varchar(6) DEFAULT NULL,
+  `first_name` varchar(65) DEFAULT NULL,
+  `last_name` varchar(65) DEFAULT NULL,
+  `mobile_number` varchar(10) DEFAULT NULL,
+  `nominee_name` varchar(65) DEFAULT NULL,
+  `nominee_relationship` varchar(65) DEFAULT NULL,
+  `permanent_address` varchar(125) DEFAULT NULL,
+  `account_holder_name` varchar(65) DEFAULT NULL,
+  `bank_name` varchar(50) DEFAULT NULL,
+  `account_number` varchar(18) DEFAULT NULL,
+  `branch` varchar(65) DEFAULT NULL,
+  `ifsc_code` varchar(18) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `product_details`
+--
+
+CREATE TABLE `product_details` (
+  `product_id` varchar(6) DEFAULT NULL,
+  `name` varchar(65) DEFAULT NULL,
+  `description` varchar(165) DEFAULT NULL,
+  `duration` varchar(12) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `product_details`
+--
+
+INSERT INTO `product_details` (`product_id`, `name`, `description`, `duration`) VALUES
+('1', 'Swarna Lakshmi ', 'This is the perfection level of chit.', '1'),
+('2', 'Swarna Thangam', 'This is the initial stage of chit.', '11'),
+('3', 'Swarna Sanjay', 'This is basic sanjay.', '4');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_details`
+--
+
+CREATE TABLE `user_details` (
+  `user_id` varchar(6) DEFAULT NULL,
+  `first_name` varchar(65) DEFAULT NULL,
+  `last_name` varchar(65) DEFAULT NULL,
+  `mobile_number` varchar(10) DEFAULT NULL,
+  `password` varchar(16) DEFAULT NULL,
+  `role` varchar(10) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `user_details`
+--
+
+INSERT INTO `user_details` (`user_id`, `first_name`, `last_name`, `mobile_number`, `password`, `role`) VALUES
+('1', 'Akaash', 'B', '6379617651', 'Akaashnuu2003#', 'admin'),
+('2', 'a', 'l', '7894561230', 'l', 'user'),
+('3', 'Divya', 'Mam', '7894561233', 'a', 'user');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
